184b5466e49f63ebfd866fd110cf587b51f49d8a
MinXiong-Huang <4a5g0083@stust.edu.tw>
2021-06-12 13:52:27 +0800

    add files

---
+++ axios.js
@@ -0,0 +1,26 @@
+const api_url = 'https://reqres.in/api/users';
+
+//Method: GET
+axios
+.get(api_url)
+.then(function (res) {
+    var infos = res.data.data;
+    console.log(infos);
+})
+.catch(function (err) {
+    console.log(err);
+})
+
+
+//Method: POST
+axios
+.post(api_url, {
+    name: "Rock",
+    job: "enginner"
+})
+.then(function (res) {
+    console.log(res);
+})
+.catch(function (err) {
+    console.log(err);
+})
