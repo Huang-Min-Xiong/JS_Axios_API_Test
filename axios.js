d644b8c21f92581080ae75b91e38cb6c81138247
黃敏雄 <48427735+Huang-Min-Xiong@users.noreply.github.com>
2021-07-01 22:25:14 +0800

    Update axios.js

--- axios.js
+++ axios.js
@@ -2,25 +2,25 @@ const api_url = 'https://reqres.in/api/users';
 
 //Method: GET
 axios
-.get(api_url)
-.then(function (res) {
-    var infos = res.data.data;
-    console.log(infos);
-})
-.catch(function (err) {
-    console.log(err);
-})
+    .get(api_url)
+    .then(function (res) {
+        var infos = res.data.data;
+        console.log(infos);
+    })
+    .catch(function (err) {
+        console.log(err);
+    })
 
 
 //Method: POST
 axios
-.post(api_url, {
-    name: "Rock",
-    job: "enginner"
-})
-.then(function (res) {
-    console.log(res);
-})
-.catch(function (err) {
-    console.log(err);
-})
+    .post(api_url, {
+        name: "Rock",
+        job: "enginner"
+    })
+    .then(function (res) {
+        console.log(res);
+    })
+    .catch(function (err) {
+        console.log(err);
+    })
